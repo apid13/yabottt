cf984c88f521d30cb3e9da82ee328c657d65a371
apid13 <78915816+apid13@users.noreply.github.com>
2021-02-12 21:14:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('DRAWL NAG\nWhatsApp Bot', {
+CFonts.say('FIDS\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'Drawl Nag BOT' By @${package.author.name || package.author}`, {
+CFonts.say(`'FIDS BOT' By @${package.author.name || package.author}`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
